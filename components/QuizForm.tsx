828fb3c127b7e2e571c36cfc9522fa5d83c7947f
Julian <julian_sunpacyo@dlsu.edu.ph>
2023-02-28 21:38:36 -0800

    Made buttons responsive
    Used Tailwind only

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -29,7 +29,7 @@ const QuizForm: React.FC<Props> = ({ isLoading, questionSet, handleNextQuestion 
       <div className="relative h-40">
         <Image src={imgUrl} alt="question" width={1200} height={200} unoptimized loading="eager" />
       </div>
-      <ul className="flex flex-col gap-2 my-5 select-none px-12 h-96">
+      <ul className="flex flex-col gap-2 mt-5 mb-16 select-none md:px-12 px-0 h-max">
         {options.map((option, index) => (
           <li key={index}>
             <SelectionInput
@@ -44,15 +44,15 @@ const QuizForm: React.FC<Props> = ({ isLoading, questionSet, handleNextQuestion 
           </li>
         ))}
       </ul>
-      <div className="flex justify-center">
+      <div className="flex justify-center flex-col sm:flex-row">
         <button
-          className="text-white bg-emerald-600/50 hover:bg-emerald-600/60 border-emerald-600 border focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-emerald-600/50 dark:hover:bg-emerald-600/60 focus:outline-none dark:focus:ring-green-800"
+          className="text-white bg-emerald-600/50 hover:bg-emerald-600/60 border-emerald-600 border focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm sm:px-5 py-2.5 sm:mr-2 mb-2 sm:mb-0 dark:bg-emerald-600/50 dark:hover:bg-emerald-600/60 focus:outline-none dark:focus:ring-green-800"
           type="submit"
         >
           Reveal Answer
         </button>
         <button
-          className="text-white bg-blue-600/50 border-blue-600 border hover:bg-blue-600/60 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600/50 dark:border-blue-600 dark:hover:bg-blue-600/60 focus:outline-none dark:focus:ring-blue-800"
+          className="text-white bg-blue-600/50 border-blue-600 border hover:bg-blue-600/60 focus:ring-1 focus:ring-blue-300 font-medium rounded-lg text-xs sm:text-sm sm:px-5 py-2.5 sm:mr-2 dark:bg-blue-600/50 dark:border-blue-600 dark:hover:bg-blue-600/60 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => {
             reset();
             setShowCorrectAnswer(false);
