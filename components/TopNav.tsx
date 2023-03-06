6c0ff8c3cd5aee5a3128ff6c9fb9ce9845877341
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-03-06 19:17:06 +0200

    Prepare header for github stars button

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const TopNav = () => {
   return (
-    <div className="h-16 mb-10 w-full bg-slate-700 text-white flex justify-center items-center flex-col">
-      <p className="font-bold text-4xl leading-7">AZURE</p>
-      <p className="text-base">FUNDAMENTALS</p>
+    <div className="h-16 mb-10 w-full px-3 border-b-2 border-slate-700 text-white flex justify-between items-center">
+      <div className="flex items-center flex-col">{/* empty div */}</div>
+      <div className="flex items-center flex-col">
+        <p className="font-bold text-4xl leading-7">AZURE</p>
+        <p className="text-base">FUNDAMENTALS</p>
+      </div>
+      <div className="flex items-center flex-col">{/* github stars button */}</div>
     </div>
   );
 };
