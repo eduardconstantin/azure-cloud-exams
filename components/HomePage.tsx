a203dc2ec2e2ca7cc52003fa59dfd05859e434a6
milafreckled <miladul2014@gmail.com>
2023-05-12 21:07:15 +0200

    Update components/HomePage.tsx
    Co-authored-by: Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
             <h2 className="text-white text-5xl text-leading font-bold uppercase mt-20">Welcome!</h2>
             <p  className="text-white text-lg mt-4 mb-12  px-2">Test your knowledge under pressure with our timed exam mode, or explore and master over 480 questions at your own pace with our practice mode.</p>
             <div className="flex max-sm:flex-col max-sm:align-center justify-center gap-4">
-            <Link href="/practice">
+            <Link href="/practice" className="group bg-slate-700 text-white hover:bg-gradient-to-r from-[#0284C7] to-[#2DD48F] rounded-lg p-[2px] w-[350px] h-[350px] cursor-pointer">
             <div className="flex flex-col justify-center h-full flex-auto bg-slate-800 rounded-lg pt-7 px-6">
                 <h2 className={`${hovered === 1 ? "bg-gradient-to-r from-[#0284C7] to-[#2DD48F] text-transparent bg-clip-text" : "" } uppercase text-2xl font-bold flex-1"`}>Practice mode</h2>
                 <p className="text-sm text-slate-400 mt-7">Learn and familiarize yourself with the questions and answers without any time constraint.</p>
