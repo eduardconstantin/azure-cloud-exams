75fef2d651cfabec7666ff1993c373b09f4efe96
milafreckled <miladul2014@gmail.com>
2023-05-12 21:05:42 +0200

    Update padding HomePage.tsx
    Co-authored-by: Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     return(
         <div className="mx-auto w-5/6 sm:w-2/5 text-center">
             <h2 className="text-white text-5xl text-leading font-bold uppercase mt-10">Welcome!</h2>
-            <p  className="text-white text-lg mt-4 mb-12">Test your knowledge under pressure with our timed exam mode, or explore and master over 480 questions at your own pace with our practice mode.</p>
+            <p  className="text-white text-lg mt-4 mb-12  px-2">Test your knowledge under pressure with our timed exam mode, or explore and master over 480 questions at your own pace with our practice mode.</p>
             <div className="flex max-sm:flex-col max-sm:align-center justify-center gap-4">
             <Link href="/practice">
             <div onMouseEnter={() => setHovered(1)} onMouseLeave={() => setHovered(0)} className="bg-slate-700 text-white hover:bg-gradient-to-r from-[#0284C7] to-[#2DD48F] rounded-lg p-[2px] w-[350px] h-[350px] cursor-pointer">
