a89065c946568cca9b5c90769bff82378ad6f049
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-05-14 10:34:33 +0300

    Fix home margin top

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="mx-auto w-5/6 sm:w-2/5 text-center">
-      <h2 className="text-white text-5xl text-leading font-bold uppercase mt-20">Welcome!</h2>
+      <h2 className="text-white text-5xl text-leading font-bold uppercase mt-14">Welcome!</h2>
       <p className="text-white text-lg mt-4 mb-14 px-5 leading-6">
         Test your knowledge under pressure with our timed exam mode, or explore and master over 480 questions at your
         own pace with our practice mode.
