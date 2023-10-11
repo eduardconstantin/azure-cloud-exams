e5598abd1f15150810e799d3ce6d6f384df0b1ea
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2023-10-11 20:14:32 +0300

    Update pages/api/og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -13,7 +13,7 @@ export default function handler(req: NextRequest) {
 
     return new ImageResponse(
       (
-        <div tw="flex text-center w-full h-full bg-slate-800 items-center justify-center mt-20 ">
+        <div tw="flex text-center w-full h-full bg-slate-800 items-center justify-center ">
           <p
             tw={`px-8 ${
               width < 640 ? "text-5xl" : "text-3xl"
