b85e4ad5d1f3046b597faba0be1726ae198c0f14
masterHAWK99 <maciej.pochron99@gmail.com>
2023-12-06 19:24:29 +0100

    Get rid of warnings

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse, NextRequest } from "next/server";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function GET(req: NextRequest) {
   try {
