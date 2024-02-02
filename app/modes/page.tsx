905fe6dcf93ad4fc5310c11ae2bcc9af0515465f
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-02-02 17:56:14 +0000

    Remove welcome title from modes page

--- app/modes/page.tsx
+++ app/modes/page.tsx
@@ -4,9 +4,6 @@ import ExamLink from "@azure-fundamentals/components/ExamLink";
 const Home: NextPage = () => {
   return (
     <div className="mx-auto w-5/6 sm:w-2/3 lg:w-2/3 xl:w-2/4 text-center">
-      <h2 className="text-white text-5xl text-leading font-bold uppercase mt-14">
-        Welcome!
-      </h2>
       <p className="text-white text-lg mt-4 mb-14 px-5 leading-6">
         Test your knowledge under pressure with our timed exam mode, or explore
         and master over 480 questions at your own pace with our practice mode.
