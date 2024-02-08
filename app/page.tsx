496786b701511fe493fc421252f6b35cc77e9825
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-02-08 17:35:31 +0000

    Add the exams list to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import NameLink from "@azure-fundamentals/components/NameLink";
+import exams from "@azure-fundamentals/lib/exams.json";
 
-const Home: NextPage = () => {
+const Home: NextPage = async () => {
   return (
     <div className="mx-auto w-5/6 sm:w-2/3 lg:w-2/3 xl:w-2/4 text-center">
       <h2 className="text-white text-5xl text-leading font-bold uppercase mt-14">
@@ -10,35 +11,19 @@ const Home: NextPage = () => {
       <p className="text-white text-lg mt-4 mb-14 px-5 leading-6">
         Select a Microsoft Azure exam from the list bellow.
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 sm:mx-16">
-        <NameLink
-          href="/modes"
-          heading="MICROSOFT AZURE AZ-900"
-          paragraph="Microsoft Azure Fundamentals"
-          wrapperClassNames="hover:bg-[#C7D2E2]"
-          headingClassNames="group-hover:from-[#fff] group-hover:to-[#fff]"
-        />
-        <NameLink
-          href="/practice"
-          heading="MICROSOFT AZURE AI-900"
-          paragraph="Microsoft Azure AI Fundamentals"
-          wrapperClassNames="hover:bg-[#C7D2E2]"
-          headingClassNames="group-hover:from-[#fff] group-hover:to-[#fff]"
-        />
-        <NameLink
-          href="/practice"
-          heading="MICROSOFT AZURE AZ-900"
-          paragraph="Microsoft Azure Fundamentals"
-          wrapperClassNames="hover:bg-[#C7D2E2]"
-          headingClassNames="group-hover:from-[#fff] group-hover:to-[#fff]"
-        />
-        <NameLink
-          href="/practice"
-          heading="MICROSOFT AZURE AZ-900"
-          paragraph="Microsoft Azure Fundamentals"
-          wrapperClassNames="hover:bg-[#C7D2E2]"
-          headingClassNames="group-hover:from-[#fff] group-hover:to-[#fff]"
-        />
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-10 gap-y-5 sm:mx-16">
+        {exams.map((exam) => {
+          return (
+            <NameLink
+              key={exam.name}
+              href="/modes"
+              heading={exam.name}
+              paragraph={exam.subtitle}
+              wrapperClassNames="hover:bg-[#C7D2E2]"
+              headingClassNames="group-hover:from-[#fff] group-hover:to-[#fff]"
+            />
+          );
+        })}
       </div>
     </div>
   );
