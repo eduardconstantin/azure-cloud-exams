e96de3a3d1eabf4121b91c91527aca3ba21a044b
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-02-16 18:42:29 +0000

    Remove images from questions header

--- lib/graphql/repoQuestions.tsx
+++ lib/graphql/repoQuestions.tsx
@@ -2,7 +2,8 @@ const url =
   "https://raw.githubusercontent.com/Ditectrev/Microsoft-Azure-AZ-900-Microsoft-Azure-Fundamentals-Exam-Questions-Answers/main/README.md";
 
 const scrapeQuestions = (markdownText: string) => {
-  const regex = /### (.*?)\n\n((?:- \[(?:x| )\] .*?\n)+)/gs;
+  const regex =
+    /### (.*?)\s*\n\n\!\[.*?\]\(.*?\)\s*\n\n((?:- \[(?:x| )\] .*?\n)+)/gs;
   const optionsRegex = /- \[(x| )\] (.*?)(?=\n- \[|$)/g;
   const questions = [];
   let match;
@@ -40,6 +41,7 @@ export const FetchQuestions = async (link: string) => {
       throw new Error(res.statusText);
     }
     const markdown = await res.text();
+
     return scrapeQuestions(markdown);
   } catch (err: any) {
     console.error(err.message);
