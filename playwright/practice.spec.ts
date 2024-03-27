f48bcca5ead24154239fa33ea4b39073fa82d453
Raboro <marius.woerfelreal@gmail.com>
2024-03-27 12:57:56 +0100

    renamed test

--- playwright/practice.spec.ts
+++ playwright/practice.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("Example", async ({ page }) => {
+test("Application should have correct initial state", async ({ page }) => {
   await page.goto("");
   expect(await page.getByRole("main").getByRole("link").all()).toHaveLength(22);
   await expect(page.getByRole("main").getByRole("heading").first()).toHaveText(
