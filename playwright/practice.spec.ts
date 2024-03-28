5c262bc4f80365a6cbe162472a5ff5da3310fbc9
Raboro <marius.woerfelreal@gmail.com>
2024-03-29 00:16:18 +0100

    added test for practice

--- playwright/practice.spec.ts
+++ playwright/practice.spec.ts
@@ -70,3 +70,59 @@ test("Application should have correct initial state", async ({ page }) => {
     await expect(link.locator("p")).toHaveText(examParagraphs[index++]);
   }
 });
+
+test("Practice should work properly", async ({ page }) => {
+  await page.goto("");
+  await page.getByRole("link", { name: "AWS Certified ANS-C01 AWS" }).click();
+
+  await expect(
+    page.getByRole("heading", { name: "AWS Certified ANS-C01" }),
+  ).toBeVisible();
+  await expect(
+    page.locator("main").getByRole("link").first().locator("h2"),
+  ).toHaveText("Practice mode");
+  await expect(
+    page.locator("main").getByRole("link").last().locator("h2"),
+  ).toHaveText("Exam mode");
+  expect(await page.locator("main").getByRole("link").all()).toHaveLength(2);
+
+  await page.locator("main").getByRole("link").last().click();
+  await expect(page.locator("main").locator("h1")).toContainText(
+    "PRACTICE EXAM",
+  );
+  await expect(
+    page.locator("main").locator("p", { hasText: "15:00" }),
+  ).toBeVisible();
+
+  const startButton = page.getByRole("button", { name: "Begin exam" });
+  await expect(startButton).toBeEnabled();
+  await startButton.click();
+
+  await expect(
+    page.locator("button").filter({ hasText: "Next Question" }),
+  ).not.toBeEnabled();
+
+  const skipQuestion = page
+    .locator("button")
+    .filter({ hasText: "Skip Question" });
+  await expect(skipQuestion).toBeEnabled();
+  await skipQuestion.click();
+
+  await page.getByRole("button").first().click();
+
+  await page.getByRole("link", { name: "AWS Certified CLF-C02 AWS" }).click();
+  await page.getByRole("link", { name: "Practice mode Learn and" }).click();
+
+  await expect(page.locator("input[type=number]")).toHaveValue("1");
+  await expect(page.getByText("597")).toBeVisible();
+  expect(await page.locator("label").all()).toHaveLength(4);
+  await page.locator("label").first().click();
+  await page.getByRole("button", { name: "Reveal Answer" }).click();
+  await expect(page.locator("main ul")).toHaveScreenshot({ threshold: 0.02 });
+
+  await page.getByRole("button", { name: "Next Question" }).click();
+  await expect(page.locator("input[type=number]")).toHaveValue("2");
+  await page.locator("label").last().click();
+  await page.getByRole("button", { name: "Reveal Answer" }).click();
+  await expect(page.locator("main ul")).toHaveScreenshot({ threshold: 0.02 });
+});
