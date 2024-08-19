f48f182d5c4cc6e6ef2070b6f4e5dc852d56d8c7
Daniel Danielecki <daniel.danielecki@mail.com>
2024-08-19 10:34:58 +0200

    fix: title text under logo on mobile devices

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -39,7 +39,7 @@ const TopNav = () => {
           height={50}
           width={200}
         />
-        <p className={`windowWidth < 640 ? "text-md" : "text-lg"`}>
+        <p className={`${windowWidth < 768 ? "text-sm" : "text-xl"}`}>
           🧪 Practice Exams Platform
         </p>
       </div>
